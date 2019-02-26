12b111263b86e38325357c99292f0eb9cc50abed
Nurikhsan <ikhsanhikari29@gmail.com>
2019-02-26 23:06:01 +0700

    init

---
+++ MultiLangSpringBoot/src/main/java/com/multiple/lang/controller/MainController2.java
@@ -0,0 +1,14 @@
+package com.multiple.lang.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController2 {
+    @RequestMapping(value = "/{locale:en|fr|vi}/login2")
+    public String login2(Model model) {
+        return "login2";
+    }
+}
